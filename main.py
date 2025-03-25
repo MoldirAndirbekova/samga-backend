7fb7e6d24c81e6f75280f10a27de7fd06a2e7748
Moldir Andirbekova <moldirandirbekova@Moldirs-MacBook-Pro.local>
2025-03-25 20:58:45 +0500

    create auth endpoints. jwt token

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
+from src.apis import apis
+from src.prisma import prisma
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await prisma.connect()
+    yield
+    await prisma.disconnect()
+
+app = FastAPI(lifespan=lifespan)
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+app.include_router(apis, prefix="/apis")
 
 @app.get("/")
-async def get_data():
-    return {"data": "some data"}
+def read_root():
+    return {"version": "1.0.0"}
---
+++ src/apis/__init__.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from src.apis.auth import router as authRouter
+
+apis = APIRouter()
+apis.include_router(authRouter)
+
+__all__ = ["apis"]
