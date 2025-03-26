8682b385bc6cdae3ea71541f910060d1d7c4c1fd
Moldir Andirbekova <moldirandirbekova@Moldirs-MacBook-Pro.local>
2025-03-26 15:42:09 +0500

    configure cors origins

--- settings.py
+++ settings.py
@@ -6,6 +6,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    CORS_ORIGINS: Set[str]
+
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
