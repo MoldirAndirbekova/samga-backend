df0451c5c8111fcd9b841dcef984743a9748f9bb
Moldir Andirbekova <moldirandirbekova@Moldirs-MacBook-Pro.local>
2025-03-28 14:13:57 +0500

    ref

--- src/apis/__init__.py
+++ src/apis/__init__.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter
 from src.apis.auth import router as authRouter
 from src.apis.game import router as gameRouter
 from src.apis.users import router as usersRouter
+
 apis = APIRouter()
+
 apis.include_router(authRouter)
 apis.include_router(gameRouter)
 apis.include_router(usersRouter)
