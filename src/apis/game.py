8d51fc7639ae4ac838c3c7848cc1730602cc9fc8
Moldir Andirbekova <moldirandirbekova@Moldirs-MacBook-Pro.local>
2025-04-04 00:16:28 +0500

    add game retrieval by id

--- src/apis/game.py
+++ src/apis/game.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from src.prisma import prisma
 from src.utils.auth import JWTBearer, decodeJWT
 from fastapi import HTTPException
 
 router = APIRouter()
+
+class Game(BaseModel):
+    id: str
+    name: str
+    image_url: str
+    category_id: str
 
 @router.get("/categories", tags=["game"])
 async def get_categories():
@@ -35,3 +42,17 @@ async def get_game(token: str = Depends(JWTBearer())):
     game = await prisma.game.find_many()
     return game
 
+@router.get("/games/{game_id}", tags=["game"], dependencies=[Depends(JWTBearer())])
+async def get_game_by_id(game_id: str, token: str = Depends(JWTBearer()), response_model=Game):
+    decoded = decodeJWT(token)
+
+    if not decoded or "userId" not in decoded:
+        raise HTTPException(status_code=401, detail="Invalid authentication token")
+
+    game = await prisma.game.find_first(
+        where={"id": game_id},
+    )
+    if game:
+        return game
+    
+    return HTTPException(status_code=404, detail="Game not found")
